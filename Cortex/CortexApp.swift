4001ffdfc095d8ade2a278f385c383c1dd24f9ac
adambdc <adam@bdcllc.io>
2026-02-19 19:45:20 -1000

    Initial Commit

---
+++ Cortex/CortexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CortexApp.swift
+//  Cortex
+//
+//  Created by Adam Carlton on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct CortexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
